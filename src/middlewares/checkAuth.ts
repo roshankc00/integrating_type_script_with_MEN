8f53b3b6a5355e08b4862c3e675ba24ca906aaaf
Roshan karki <rohitkc8848@gmail.com>
2023-06-09 20:08:18 +0545

    fix:fixing the auth middleware

--- src/middlewares/checkAuth.ts
+++ src/middlewares/checkAuth.ts
@@ -1,10 +1,11 @@
 
 import jwt,{JwtPayload,Secret} from 'jsonwebtoken'
 import UserModel from '../models/user';
+import asyncHandler from 'express-async-handler'
 import { Request,Response,NextFunction,RequestHandler } from 'express';
 import env from '../utility/validateEnv'
 
-export const authMiddleware:RequestHandler=async(req:Request,res:Response,next:NextFunction):Promise<Response | void>=>{
+export const authMiddleware:RequestHandler=asyncHandler(async(req:Request,res:Response,next:NextFunction)=>{
     let token;
     if(req.headers?.authorization?.startsWith("Bearer")){
         token=req.headers.authorization.split(" ")[1]
@@ -12,10 +13,10 @@ export const authMiddleware:RequestHandler=async(req:Request,res:Response,next:N
             if(token){
                 const decorded:any=jwt.verify(token,env.SECRET)
                 console.log(decorded.id)
-                const user=await UserModel.findById(decorded?.id).exec()
+                const user=await UserModel.findById(decorded?.id).select("+email +password").exec()
                 console.log(user)
                 // req.user=user
-                next()
+                // next()
             }
         }
         catch(error){
@@ -26,7 +27,7 @@ export const authMiddleware:RequestHandler=async(req:Request,res:Response,next:N
         throw new Error("There is no token attach to the header")
 
     }
-}
+})
 
 
 
